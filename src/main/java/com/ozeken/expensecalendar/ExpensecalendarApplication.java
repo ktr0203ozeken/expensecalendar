1293235ecda575f0f130dbc750b44ad792ed9bfa
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-04-27 13:54:09 +0900

    Initial commit

---
+++ src/main/java/com/ozeken/expensecalendar/ExpensecalendarApplication.java
@@ -0,0 +1,13 @@
+package com.ozeken.expensecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensecalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensecalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozeken/expensecalendar/ExpensecalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozeken.expensecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensecalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
