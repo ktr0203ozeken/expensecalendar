b28032b7ddde83dd7cadceddf3d40d1775fd9637
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-04-28 20:20:31 +0900

    ExpenseFormのバリデーション追加、修正

--- src/main/java/com/ozeken/expensecalendar/form/ExpenseForm.java
+++ src/main/java/com/ozeken/expensecalendar/form/ExpenseForm.java
@@ -2,6 +2,9 @@ package com.ozeken.expensecalendar.form;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -18,15 +21,18 @@ public class ExpenseForm {
 	private LocalDate date;
 
 	// カテゴリ
-	@NotNull(message = "カテゴリを入力してください")
+	@NotBlank(message = "カテゴリを入力してください")
 	@Size(max = 50, message = "カテゴリは50文字以内で入力してください")
 	private String category;
 
 	// 金額
 	@NotNull(message = "金額を入力してください")
+	@Min(value = 1, message = "金額は1以上で入力してください")
+	@Max(value = 2147483647, message = "金額は2147483647以下で入力してください")
 	private Integer amount;
 
 	// 説明
+	@Size(max = 255, message = "説明は255文字以内で入力してください")
 	private String description;
 
 }
