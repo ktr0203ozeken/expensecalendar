0b165e47a5d83acddb5eb40ff86b06aefeccefa1
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-04-28 17:53:01 +0900

    ExpenseとExpenseFormの変換を行うヘルパークラスの作成

---
+++ src/main/java/com/ozeken/expensecalendar/helper/ExpenseHelper.java
@@ -0,0 +1,34 @@
+package com.ozeken.expensecalendar.helper;
+
+import com.ozeken.expensecalendar.entity.Expense;
+import com.ozeken.expensecalendar.form.ExpenseForm;
+
+/**
+ * ExpenseとExpenseFormの変換を行うヘルパークラス
+ */
+public class ExpenseHelper {
+
+    /**
+     *  ExpenseForm → Expense への変換 
+     */
+    public static Expense convertExpense(ExpenseForm form) {
+        Expense expense = new Expense();
+        expense.setId(form.getId());
+        expense.setDate(form.getDate());
+        expense.setCategory(form.getCategory());
+        expense.setAmount(form.getAmount());
+        return expense;
+    }
+
+    /** 
+     * Expense → ExpenseForm への変換 
+     */
+    public static ExpenseForm convertExpenseForm(Expense expense) {
+        ExpenseForm form = new ExpenseForm();
+        form.setId(expense.getId());
+        form.setDate(expense.getDate());
+        form.setCategory(expense.getCategory());
+        form.setAmount(expense.getAmount());
+        return form;
+    }
+}
