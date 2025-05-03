2a3aa1d2083b5bcf7e20cce4d051d6c2a63819a7
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-05-03 15:03:24 +0900

    H2データベース導入およびテスト環境の設定

--- src/test/java/com/ozeken/expensecalendar/service/ExpenseServiceImplTest.java
+++ src/test/java/com/ozeken/expensecalendar/service/ExpenseServiceImplTest.java
@@ -8,10 +8,12 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.ozeken.expensecalendar.entity.Expense;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseServiceImplTest {
 
 	@Autowired
