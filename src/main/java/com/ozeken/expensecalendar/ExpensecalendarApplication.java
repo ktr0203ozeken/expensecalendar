0d5cb280a0669f24f973ac39be6f7b8ca2c61f29
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-05-03 15:07:21 +0900

    起動時に使用中プロファイルを標準出力に出す処理を追加

--- src/main/java/com/ozeken/expensecalendar/ExpensecalendarApplication.java
+++ src/main/java/com/ozeken/expensecalendar/ExpensecalendarApplication.java
@@ -8,6 +8,10 @@ public class ExpensecalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensecalendarApplication.class, args);
+		
+		// プロファイル確認用ログ
+        String profile = System.getProperty("spring.profiles.active");
+        System.out.println(">>> 起動時プロファイル: " + profile);
 	}
 
 }
