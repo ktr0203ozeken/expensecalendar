3956656324fda2f9afe8fddd1706b28cce033109
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-05-06 22:45:49 +0900

    chore: schema.sqlとdata.sqlをuser_id対応に更新

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,21 @@
-INSERT INTO expenses (date, category, amount, description)
-VALUES 
-('2025-04-02', '食費', 1000, 'ランチ代'),
-('2025-04-17', '交通費', 500, 'バス代'),
-('2025-04-19', '娯楽', 2000, '映画チケット'),
-
-('2025-05-01', '交通費', 1000, 'ランチ代'),
-('2025-05-04', '食費', 500, 'バス代'),
-('2025-05-15', '娯楽', 2000, '映画チケット'),
-
-('2025-06-08', '娯楽', 1000, 'ランチ代'),
-('2025-06-13', '交通費', 500, 'バス代'),
-('2025-06-25', '食費', 2000, '映画チケット');
-
 -- ユーザーデータの挿入
-INSERT INTO users (username, password, role)
+INSERT INTO users (id, username, password, role)
 VALUES 
-('user1','$2a$10$er93XnfZ3P62/.KRbXLrf.9wdZaIO35n6sQHHdB6jhrGCqqX0sb0u', 'USER'),
-('user2','$2a$10$OtijxZbUIfomg8p3w0gDyey50F6I41elanLP.tiC.vqp2l2BQa6TW', 'USER'),
-('user3',' $2a$10$aCOrvlmjb9R7u6hjAbw8O.ht3mRLBVcjJfPdGjLlA2duKdrANMMBG', 'USER')
+(1,'user1','$2a$10$er93XnfZ3P62/.KRbXLrf.9wdZaIO35n6sQHHdB6jhrGCqqX0sb0u', 'USER'),
+(2,'user2','$2a$10$OtijxZbUIfomg8p3w0gDyey50F6I41elanLP.tiC.vqp2l2BQa6TW', 'USER'),
+(3,'user3','$2a$10$aCOrvlmjb9R7u6hjAbw8O.ht3mRLBVcjJfPdGjLlA2duKdrANMMBG', 'USER')
 ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO expenses (user_id,date, category, amount, description)
+VALUES 
+(1,'2025-04-02', '食費', 1000, 'ランチ代'),
+(1,'2025-04-17', '交通費', 500, 'バス代'),
+(1,'2025-04-19', '娯楽', 2000, '映画チケット'),
+
+(1,'2025-05-01', '交通費', 1000, 'ランチ代'),
+(1,'2025-05-04', '食費', 500, 'バス代'),
+(1,'2025-05-15', '娯楽', 2000, '映画チケット'),
+
+(1,'2025-06-08', '娯楽', 1000, 'ランチ代'),
+(1,'2025-06-13', '交通費', 500, 'バス代'),
+(1,'2025-06-25', '食費', 2000, '映画チケット');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,18 @@
--- 支出テーブルを作成
-CREATE TABLE IF NOT EXISTS expenses (
-    id               BIGSERIAL PRIMARY KEY,  --  ID
-    date           DATE NOT NULL,                --  日付
-    category    VARCHAR(50) NOT NULL,  --  カテゴリ
-    amount      INTEGER NOT NULL,           --  金額
-    description TEXT,                                    --  説明
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE --  ユーザーID
-);
-
+--ユーザーテーブルを作成
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) DEFAULT 'USER'
+);
+
+-- 支出テーブルを作成
+CREATE TABLE IF NOT EXISTS expenses (
+    id          BIGSERIAL PRIMARY KEY,           -- ID
+    user_id     INTEGER NOT NULL,                -- ユーザーID（外部キー）
+    date        DATE NOT NULL,                       -- 日付
+    category    VARCHAR(50) NOT NULL,      -- カテゴリ
+    amount      INTEGER NOT NULL,               -- 金額
+    description TEXT,                                       -- 説明
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
