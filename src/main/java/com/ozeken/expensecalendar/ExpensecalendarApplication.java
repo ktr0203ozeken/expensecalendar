326081d697c5031b1638d891ce95ccd7b014918c
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-05-19 20:49:48 +0900

    fix: プロファイル出力の削除とテストプロファイルの設定
    - mainメソッド内の起動時プロファイル出力ログを削除
    - 本番用プロパティからHibernateの方言設定を削除
    - CalendarViewControllerTestに@ActiveProfiles("test")を追加し、テスト時のプロファイルを明示的に設定

--- src/main/java/com/ozeken/expensecalendar/ExpensecalendarApplication.java
+++ src/main/java/com/ozeken/expensecalendar/ExpensecalendarApplication.java
@@ -8,10 +8,5 @@ public class ExpensecalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpensecalendarApplication.class, args);
-		
-		// プロファイル確認用ログ
-        String profile = System.getProperty("spring.profiles.active");
-        System.out.println(">>> 起動時プロファイル: " + profile);
 	}
-
 }
