d594d54349f312d4ffa2a56ab1f4dc9408fb9c7e
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-05-19 20:53:20 +0900

    fix: プロファイル出力の削除とテストプロファイルの設定 (#14)
    - mainメソッド内の起動時プロファイル出力ログを削除
    - 本番用プロパティからHibernateの方言設定を削除
    - CalendarViewControllerTestに@ActiveProfiles("test")を追加し、テスト時のプロファイルを明示的に設定

--- src/test/java/com/ozeken/expensecalendar/controller/CalendarViewControllerTest.java
+++ src/test/java/com/ozeken/expensecalendar/controller/CalendarViewControllerTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.ozeken.expensecalendar.service.ExpenseService;
 
+@ActiveProfiles("test")
 @WebMvcTest(CalendarViewController.class)
 public class CalendarViewControllerTest {
 
